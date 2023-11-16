281ae56d4a0b4e4b4b8573bf7d87e653cb797f3d
Tom <148517271+arithtom@users.noreply.github.com>
2023-11-16 17:57:59 +0800

    add backup api source

--- src/domain/prices.ts
+++ src/domain/prices.ts
@@ -13,6 +13,17 @@ export async function getChartPricesFromBinance(symbol: string, period: string, 
     });
   } catch (error) {
     console.log(`Error fetching data: ${error}`);
+    const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}USDT&interval=${period}&limit=${limit}`);
+    const prices = await response.json();
+    return prices.map((price: any) => {
+      return {
+        time: price[0] / 1000,
+        open: Number(price[1]),
+        close: Number(price[4]),
+        high: Number(price[2]),
+        low: Number(price[3]),
+      }
+    });
   }
 }
 
@@ -23,6 +34,9 @@ export async function getCurrentPriceOfToken(symbol: string) {
     return data.value;
   } catch (e) {
     console.log(`Error fetching data: ${e}`);
+    const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`);
+    const data = await response.json();
+    return data.price;
   }
 }
 
@@ -40,5 +54,15 @@ export async function get24HrFromBinance(symbol: string) {
     return null;
   } catch (e) {
     console.log(`Error fetching data: ${e}`);
+    const res = await fetch(`https://api.binance.com/api/v3/ticker/24hr?symbol=${symbol}USDT`);
+    const data = await res.json();
+    if (data) {
+      return {
+        priceChangePercent: data.priceChangePercent,
+        highPrice: data.highPrice,
+        lowPrice: data.lowPrice
+      }
+    }
+    return null;
   }
 }
